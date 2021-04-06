5878954ea9d06da8208590235eab578dcd468360
Caleb Carlson <ccarlson355@gmail.com>
2021-04-05 19:33:20 -0600

    run script new

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sbt compile && sbt run
+sbt compile && spark-submit \
+  --class org.sustain.Main  \
+  --master spark://lattice  \
+  target/scala-2.12/transfer-learning_2.12-1.0.jar
