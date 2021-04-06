ceb71eab20476233fa5a7bbe1fb81f6a4d2b739b
Caleb Carlson <ccarlson355@gmail.com>
2021-04-06 09:39:30 -0600

    withCenters1

--- src/main/scala/org/sustain/Main.scala
+++ src/main/scala/org/sustain/Main.scala
@@ -17,6 +17,7 @@ import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
 import org.apache.spark.ml.feature.{MinMaxScaler, MinMaxScalerModel}
 import org.apache.spark.sql.RowFactory
 import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.{ArrayType, DataTypes, FloatType}
 
 import java.util
 import java.util.List
@@ -58,7 +59,7 @@ object Main {
 
     import sparkConnector.implicits._
 
-    /* Read collection into a DataSet, dropping null rows
+    /* Read collection into a DataSet[Row], dropping null rows
       +--------+-----------------------+
       | GISJOIN|median_household_income|
       +--------+-----------------------+
@@ -79,6 +80,28 @@ object Main {
     collection.show(10)
 
     /* Assemble features into single column
+      +--------+-----------------------+---------+
+      | GISJOIN|median_household_income| features|
+      +--------+-----------------------+---------+
+      |G2100890|                43808.0|[43808.0]|
+      |G2101610|                39192.0|[39192.0]|
+      |G1300530|                48684.0|[48684.0]|
+      |G2500190|                83546.0|[83546.0]|
+      |G2102250|                37445.0|[37445.0]|
+      |G2101770|                38835.0|[38835.0]|
+      |G3900210|                50214.0|[50214.0]|
+      |G2100670|                48779.0|[48779.0]|
+      |G3901690|                49241.0|[49241.0]|
+      |G3900170|                56253.0|[56253.0]|
+      +--------+-----------------------+---------+
+     */
+    val assembler: VectorAssembler = new VectorAssembler()
+      .setInputCols(FEATURES)
+      .setOutputCol("features")
+    val withFeaturesAssembled: Dataset[Row] = assembler.transform(collection)
+    withFeaturesAssembled.show(10)
+
+    /* Normalize features
       +--------+--------------------+
       | GISJOIN|            features|
       +--------+--------------------+
@@ -94,13 +117,6 @@ object Main {
       |G3900170|[0.35486339856616...|
       +--------+--------------------+
      */
-    val assembler: VectorAssembler = new VectorAssembler()
-      .setInputCols(FEATURES)
-      .setOutputCol("features")
-    val withFeaturesAssembled: Dataset[Row] = assembler.transform(collection)
-    withFeaturesAssembled.show(10)
-
-    // Normalize features
     val minMaxScaler: MinMaxScaler = new MinMaxScaler()
       .setInputCol("features")
       .setOutputCol("normalized_features")
@@ -109,42 +125,64 @@ object Main {
     normalizedFeatures = normalizedFeatures.drop("features")
     normalizedFeatures = normalizedFeatures.withColumnRenamed("normalized_features", "features")
       .select("GISJOIN", "features")
-
     println(">>> With normalized features:\n")
     normalizedFeatures.show(10)
 
-    // KMeans clustering
+    /* KMeans clustering centers
+      [0.4287200541151439]
+      [0.21934450125060204]
+      [0.30615526209887767]
+      [0.1292813510233636]
+      [0.6361498632909213]
+     */
     val kMeans: KMeans = new KMeans().setK(K).setSeed(1L)
     val kMeansModel: KMeansModel = kMeans.fit(normalizedFeatures)
     val centers: Array[Vector] = kMeansModel.clusterCenters
     println(">>> Cluster centers:\n")
     centers.foreach { println }
 
-    // Get cluster predictions
+    /* Get cluster predictions
+      +--------+--------------------+----------+
+      | GISJOIN|            features|prediction|
+      +--------+--------------------+----------+
+      |G2100890|[0.23429567913195...|         1|
+      |G2101610|[0.18957566363107...|         1|
+      |G1300530|[0.2815345863204805]|         2|
+      |G2500190|[0.6192792094555318]|         4|
+      |G2102250|[0.17265064909901...|         3|
+      |G2101770|[0.18611703158302...|         1|
+      |G3900210|[0.29635729509784...|         2|
+      |G2100670|[0.28245495059097...|         2|
+      |G3901690|[0.28693082735903...|         2|
+      |G3900170|[0.35486339856616...|         2|
+      +--------+--------------------+----------+
+     */
     var predictions: Dataset[Row] = kMeansModel.transform(normalizedFeatures)
     println(">>> Predictions centers:\n")
-
     predictions.show(10)
 
-    /*
-    +--------+-----------------------+--------------------+----------+
-    | GISJOIN|median_household_income|            features|prediction|
-    +--------+-----------------------+--------------------+----------+
-    |G2100890|                43808.0|[0.23429567913195...|         1|
-    |G2101610|                39192.0|[0.18957566363107...|         1|
-    |G1300530|                48684.0|[0.2815345863204805]|         2|
-    |G2500190|                83546.0|[0.6192792094555318]|         4|
-    |G2102250|                37445.0|[0.17265064909901...|         3|
-    |G2101770|                38835.0|[0.18611703158302...|         1|
-    |G3900210|                50214.0|[0.29635729509784...|         2|
-    |G2100670|                48779.0|[0.28245495059097...|         2|
-    |G3901690|                49241.0|[0.28693082735903...|         2|
-    |G3900170|                56253.0|[0.35486339856616...|         2|
-    +--------+-----------------------+--------------------+----------+
+    /* Add "center" column
+      +--------+--------------------+----------+------+
+      | GISJOIN|            features|prediction|center|
+      +--------+--------------------+----------+------+
+      |G2100890|[0.23429567913195...|         1|     1|
+      |G2101610|[0.18957566363107...|         1|     1|
+      |G1300530|[0.2815345863204805]|         2|     2|
+      |G2500190|[0.6192792094555318]|         4|     4|
+      |G2102250|[0.17265064909901...|         3|     3|
+      |G2101770|[0.18611703158302...|         1|     1|
+      |G3900210|[0.29635729509784...|         2|     2|
+      |G2100670|[0.28245495059097...|         2|     2|
+      |G3901690|[0.28693082735903...|         2|     2|
+      |G3900170|[0.35486339856616...|         2|     2|
+      +--------+--------------------+----------+------+
      */
-
     println(">>> With Center")
-    predictions = predictions.withColumn("center", col("prediction"))
+    val arrayCol: ArrayType = DataTypes.createArrayType(FloatType)
+    predictions = predictions.withColumn("center", col("features"))
+    var withCenters = predictions.map(_.getAs[Vector]("features").toArray)
+    withCenters.show(10)
+
 
 //    val withCenters: Dataset[(String, util.List[Float], Int, Vector)] = predictions.map(row => {
 //      val prediction = row.getInt(4)
@@ -169,7 +207,6 @@ object Main {
     })
     */
 
-    predictions.show(10)
   }
 
 }
