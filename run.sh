88ae6b9317b633bcf9f312bb1fc0a1d15df1578a
Sustain Acc <ccarlson355@gmail.com>
2021-04-22 09:38:12 -0600

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 sbt compile && sbt package && spark-submit \
   --class org.sustain.Main  \
-  --master spark://lattice-150:8079  \
+  --master spark://lattice-100:8079  \
   --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1 \
   target/scala-2.12/transfer-learning_2.12-1.0.jar
