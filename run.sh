424e497b7f86d8ba725da8fe145f7415c3c80091
Sustain Services Repository <ccarlson355@gmail.com>
2021-04-22 11:47:30 -0600

    Add config to run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 sbt compile && sbt package && spark-submit \
   --class org.sustain.Main  \
+  --config spark.executorEnv.JAVA_HOME=$JAVA_HOME \
   --master spark://lattice-100:8079  \
   --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1 \
   target/scala-2.12/transfer-learning_2.12-1.0.jar
