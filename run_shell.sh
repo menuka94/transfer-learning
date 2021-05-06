aab1b037c8dc5368ef07ced743b05f51b7a763de
Caleb Carlson <ccarlson355@gmail.com>
2021-05-05 18:43:34 -0600

    remove keepalive from shell script

--- run_shell.sh
+++ run_shell.sh
@@ -13,6 +13,5 @@ spark-shell \
   --conf spark.mongodb.input.uri=mongodb://lattice-100:27018/ \
   --conf spark.mongodb.input.database=sustaindb \
   --conf spark.mongodb.input.collection=noaa_nam \
-  --conf mongodb.keep_alive_ms, 100000 \
   --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1
 
