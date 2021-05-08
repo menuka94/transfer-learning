58a65a3f00d120455747b1b6433be30fda15c8be
Sustain Services Repository <ccarlson355@gmail.com>
2021-05-08 08:54:50 -0600

    Add vector disassembler JAR

--- run_shell.sh
+++ run_shell.sh
@@ -11,5 +11,6 @@ spark-shell \
   --conf spark.mongodb.input.uri=mongodb://lattice-100:27018/ \
   --conf spark.mongodb.input.database=sustaindb \
   --conf spark.mongodb.input.collection=noaa_nam \
+  --jars vectordisassember_2.12-1.0.jar \
   --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1
 
