59b702494cb571d159224cc7275e36de094c0fa8
Caleb Carlson <ccarlson355@gmail.com>
2021-05-10 10:49:25 -0600

    Use array instead of list

--- src/main/scala/org/sustain/PCACluster.scala
+++ src/main/scala/org/sustain/PCACluster.scala
@@ -3,14 +3,14 @@ package org.sustain
 class PCACluster {
 
   var clusterId: Int = -1
-  var centerGisJoin: String = null
-  var clusterGisJoins: List[String] = List[String]()
+  var centerGisJoin: String = ""
+  var clusterGisJoins: Array[String] = Array[String]()
 
   /**
    * Overrides the toString method, for debugging model queues
    * @return String representation of PCACluster
    */
   override def toString: String = {
-    "%d:%s:%s".format(clusterId, centerGisJoin, clusterGisJoins.toString())
+    "%d:%s:%s".format(clusterId, centerGisJoin, clusterGisJoins.mkString("Array(", ", ", ")"))
   }
 }
