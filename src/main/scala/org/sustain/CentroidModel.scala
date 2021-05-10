2a986912f3b1abd5030ae2f50267a8a158e801e8
Caleb Carlson <ccarlson355@gmail.com>
2021-05-10 11:26:19 -0600

    Try train centers again

--- src/main/scala/org/sustain/CentroidModel.scala
+++ src/main/scala/org/sustain/CentroidModel.scala
@@ -47,39 +47,35 @@ class CentroidModel(sparkMasterC: String, mongoHostC: String, mongoPortC: String
       +--------+-------------------+--------+-------------------------+
       |gis_join|year_month_day_hour|timestep|temp_surface_level_kelvin|
       +--------+-------------------+--------+-------------------------+
-      |G4804230|         2010010100|       0|        281.4640808105469|
-      |G5600390|         2010010100|       0|        265.2140808105469|
-      |G1701150|         2010010100|       0|        265.7140808105469|
-      |G0601030|         2010010100|       0|        282.9640808105469|
-      |G3701230|         2010010100|       0|        279.2140808105469|
-      |G3700690|         2010010100|       0|        280.8390808105469|
-      |G3701070|         2010010100|       0|        280.9640808105469|
-      |G4803630|         2010010100|       0|        275.7140808105469|
-      |G5108200|         2010010100|       0|        273.4640808105469|
-      |G4801170|         2010010100|       0|        269.3390808105469|
+      |G1200870|         2010011000|       0|       297.02488708496094|
+      |G1200870|         2010011000|       0|       280.64988708496094|
+      |G1200870|         2010011000|       0|       287.02488708496094|
+      |G1200870|         2010011000|       0|       279.02488708496094|
+      |G1200870|         2010011000|       0|       296.14988708496094|
+      |G1200870|         2010011000|       0|       279.02488708496094|
+      |G1200870|         2010011000|       0|       291.77488708496094|
+      |G1200870|         2010011000|       0|       283.64988708496094|
+      |G1200870|         2010011000|       0|       279.02488708496094|
+      |G1200870|         2010011000|       0|       286.77488708496094|
+      ...
       +--------+-------------------+--------+-------------------------+
      */
     var mongoCollection: Dataset[Row] = MongoSpark.load(sparkSession)
     mongoCollection = mongoCollection.select("gis_join", "year_month_day_hour", "timestep", "temp_surface_level_kelvin")
-      .na.drop()
-    mongoCollection.show(10)
-
-    // Filter the data down to just entries for a single GISJoin
-    var gisJoinCollection: Dataset[Row] = mongoCollection.filter(
+      .na.drop().filter(
       col("gis_join") === this.gisJoin && col("timestep") === 0
-      ).withColumnRenamed(this.label, "label")
-
-    gisJoinCollection.show(20)
+    ).withColumnRenamed(this.label, "label")
+    mongoCollection.show(10)
 
     val assembler: VectorAssembler = new VectorAssembler()
       .setInputCols(this.features)
       .setOutputCol("features")
-    gisJoinCollection = assembler.transform(gisJoinCollection)
-    gisJoinCollection.show(20)
+    mongoCollection = assembler.transform(mongoCollection)
+    mongoCollection.show(11)
 
     // Split input into testing set and training set:
     // 80% training, 20% testing, with random seed of 42
-    val Array(train, test): Array[Dataset[Row]] = gisJoinCollection.randomSplit(Array(0.8, 0.2), 42)
+    val Array(train, test): Array[Dataset[Row]] = mongoCollection.randomSplit(Array(0.8, 0.2), 42)
 
     // Create a linear regression model object and fit it to the training set
     val lrModel: LinearRegressionModel = this.linearRegression.fit(train)
@@ -88,6 +84,8 @@ class CentroidModel(sparkMasterC: String, mongoHostC: String, mongoPortC: String
     val lrPredictions: DataFrame = lrModel.transform(test)
     val evaluator: RegressionEvaluator = new RegressionEvaluator().setMetricName("rmse")
     println("\n\n>>> Test set RMSE for " + this.gisJoin + ": " + evaluator.evaluate(lrPredictions))
+
+    sparkSession.close()
   }
 
   /**
