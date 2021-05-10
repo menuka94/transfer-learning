f2be02117c0a99de7e4872d530ad35d3d5455a5c
Caleb Carlson <ccarlson355@gmail.com>
2021-05-10 10:52:35 -0600

    Use mutable Array instead of list

--- src/main/scala/org/sustain/PCACluster.scala
+++ src/main/scala/org/sustain/PCACluster.scala
@@ -1,16 +1,18 @@
 package org.sustain
 
+import scala.collection.mutable.ArrayBuffer
+
 class PCACluster {
 
   var clusterId: Int = -1
   var centerGisJoin: String = ""
-  var clusterGisJoins: Array[String] = Array[String]()
+  var clusterGisJoins: ArrayBuffer[String] = ArrayBuffer[String]()
 
   /**
    * Overrides the toString method, for debugging model queues
    * @return String representation of PCACluster
    */
   override def toString: String = {
-    "%d:%s:%s".format(clusterId, centerGisJoin, clusterGisJoins.mkString("Array(", ", ", ")"))
+    "%d:%s:%s".format(clusterId, centerGisJoin, clusterGisJoins.toString())
   }
 }
