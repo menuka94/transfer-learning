d87da253207341f7bd6fda18aab20138775a3d3f
Caleb Carlson <ccarlson355@gmail.com>
2021-05-14 13:16:42 -0600

    driver memory ?

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ sbt compile && sbt package && spark-submit \
   --conf spark.executorEnv.JAVA_HOME=$JAVA_HOME \
   --executor-memory 16G \
   --executor-memory 16G \
+  --driver-memory 8G \
   --master spark://lattice-100:8079  \
   --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1 \
   target/scala-2.12/transfer-learning_2.12-1.0.jar
