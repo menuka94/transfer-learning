5af8b99cf572d68f30959000db940a0e14143d36
Caleb Carlson <ccarlson355@gmail.com>
2021-05-15 11:25:49 -0600

    Print params?3

--- src/main/scala/org/sustain/TransferLR.scala
+++ src/main/scala/org/sustain/TransferLR.scala
@@ -247,8 +247,8 @@ class TransferLR {
     // Run cross-validation, and choose the best set of parameters.
     val crossValidatorModel: CrossValidatorModel = crossValidator.fit(train)
 
-    println("\n\n>>> Params:")
-    crossValidatorModel.bestModel.params.foreach{println}
+    println("\n\n>>> Params: %s".format(crossValidatorModel.estimatorParamMaps.toString()))
+
 
     // Make predictions on the testing Dataset, evaluate performance
     val predictions: Dataset[Row] = crossValidatorModel.transform(test)
